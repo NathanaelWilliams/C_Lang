b8112ca3cf41322e28dc628621f5bdd623905644
NathanaelWilliams <123523446+NathanaelWilliams@users.noreply.github.com>
2024-02-13 20:49:05 +0800

    Update sample1.c

--- sample1.c
+++ sample1.c
@@ -2,5 +2,14 @@
 #include <stdlib.h>
 
 int main(){
-
+  /* Although the better form is:
+   int main()
+   {
+      return 0;
+   }
+   I get used to using that form 
+   because of Java
+*/
+  printf("HelloWorld\n");
+  return 0;
 }
