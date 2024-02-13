71263ea31177dd49868f10bfaf760d7ee61569d6
NathanaelWilliams <123523446+NathanaelWilliams@users.noreply.github.com>
2024-02-13 21:27:59 +0800

    Create sample3.c

---
+++ sample3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  
+  printf("%d",sizeof(int));
+  printf("%d",sizeof(double));
+  printf("%d",sizeof(char));
+  printf("%d",sizeof(float));
+  
+  return 0;
+}
